96d6cf4e28affd36164d3c52c7e252a1a56e848d
Matt-Camerato <matt.camerato@gmail.com>
2023-02-27 09:28:05 -0500

    New AudioSystem
    Audio system combines old music and SFX systems and will exist across both scenes. Moving onto animating UI and finishing up main menu functionality.

---
+++ Assets/Scripts/AudioSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSystem : MonoBehaviour
+{
+    public static AudioSystem Instance;
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioSource tankSource;
+
+    [Header("SFX Clips")]
+    [SerializeField] private AudioClip button;
+    [SerializeField] private AudioClip damage;
+    [SerializeField] private AudioClip fire;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void UpdateMusicVolume(float value)
+    {
+        musicSource.volume = value;
+    }
+
+    public void UpdateSFXVolume(float value)
+    {
+        sfxSource.volume = value;
+        tankSource.volume = value;
+    }
+
+    public void ButtonSFX() => sfxSource.PlayOneShot(button);
+    public void DamageSFX() => sfxSource.PlayOneShot(damage);
+    public void FireSFX() => sfxSource.PlayOneShot(fire);
+
+    public void ToggleTankNoise()
+    {
+        if(tankSource.isPlaying) tankSource.Stop();
+        else tankSource.Play();
+    }
+}

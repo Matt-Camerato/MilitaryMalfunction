da062abf0b70e8bdf05bc794e13ab7e00474ce90
Matt-Camerato <matt.camerato@gmail.com>
2023-03-01 11:54:54 -0500

    Most of main menu functionality
    Added all UI animations and programmed functionality for almost everything. Had to start creating the new save system for the new game and continue game buttons, and now just need to hook everything up and finish the validity check for player name input.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Animator))]
 public class MainMenuManager : MonoBehaviour
 {
-    //[Header("UI Elements")]
-    //[SerializeField] private
+    [Header("UI Elements")]
+    [SerializeField] private Button continueGameButton;
+    [SerializeField] private Button nameInputContinueButton;
+    [SerializeField] private InputField nameInputField;
 
     private Animator anim;
+    private bool hasSaveInfo = false;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+
+        //see if player has save info or not and set interactablity of continue button
+        hasSaveInfo = !PlayerPrefs.GetString("SaveData", "null").Equals("null");
+        continueGameButton.interactable = hasSaveInfo;
     }
 
-    public void Quit()
+    //called at the end of each screen fade to transition to the game scene
+    public void StartGame()
     {
-        
+        //put scene transition here
     }
+
+    //home button methods
+    public void NewGameButton() => anim.SetTrigger("HomeToNameInput");
+    public void ContinueGameButton() => anim.SetTrigger("HomeToFade");
+    public void SettingsButton() => anim.SetTrigger("HomeToSettings");
+
+    //name input methods
+    public void OnNameInputChanged()
+    {
+        //check if name input is valid
+
+        //update interactability of continue button
+        //nameInputContinueButton.interactable = 
+    }
+    public void NameInputReturnButton() => anim.SetTrigger("NameInputToHome");
+    public void NameInputContinueButton()
+    {
+        //initialize save data with player's name and wave number starting at 1
+        string saveData = nameInputField.text + "_1";
+        PlayerPrefs.SetString("SaveData", saveData);
+
+        //fade out before transitioning to game scene
+        anim.SetTrigger("NameInputToFade");
+    }
+
+    //settings methods
+    public void OnMusicSliderChanged(float value) => AudioSystem.Instance.UpdateMusicVolume(value);
+    public void OnSFXSliderChanged(float value) => AudioSystem.Instance.UpdateSFXVolume(value);
+    public void SettingsReturnButton() => anim.SetTrigger("SettingsToHome");
 }
